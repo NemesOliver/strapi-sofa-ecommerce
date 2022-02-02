bb8f5d58bc9c7082b0a194602c662337bf4d000c
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-02-02 17:56:49 +0000

    feat: fetch all products from CMS

--- pages/shop/products.js
+++ pages/shop/products.js
@@ -1,5 +1,41 @@
-const products = () => {
+import url from "../../strapi_url/url";
+
+const products = ({ products }) => {
+  console.log(products);
+
   return <div>products all</div>;
 };
 
 export default products;
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch(`${url}/products?populate=*`);
+    const { data } = await res.json();
+
+    // Clean-up response
+    const products = data.map(({ id, attributes }) => ({
+      id,
+      product_description: attributes.product_description,
+      product_in_stock: attributes.product_in_stock,
+      product_name: attributes.product_name,
+      product_price: attributes.product_price,
+      product_img: attributes.product_img.data.map((img) => ({
+        name: img.attributes.name,
+        url: img.attributes.url,
+      })),
+      categories: attributes.categories.data.map((category) => ({
+        id: category.id,
+        name: category.attributes.name,
+      })),
+    }));
+
+    return {
+      props: { products },
+    };
+  } catch (e) {
+    console.warn(e.message);
+
+    return { props: {} };
+  }
+}
