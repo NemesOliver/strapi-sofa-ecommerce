009f926f1f3333c50fda54c6b76cd0be81013e59
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-02-05 20:22:24 +0000

    feat: add animation

--- components/product/libs/color_picker/ColorPicker.js
+++ components/product/libs/color_picker/ColorPicker.js
@@ -1,11 +1,13 @@
 import { useMemo } from "react";
 
 export const ColorPicker = ({ changeImage, colors }) => {
+  // Map over pictures from response, and set colors of color picker square based on picuters
+  // Color must be set in CMS under 'caption' for this to work, example: caption: #<color>
   const classNames = useMemo(
     () =>
       colors.map(
         ({ color }) =>
-          `w-[60px] h-[60px] bg-[${color}] mx-2 sm:mr-4 rounded-[50%] hover:rounded-md transition-all duration-300 ease-linear`
+          `w-[60px] h-[60px] bg-[${color}] mx-2 sm:mr-4 rounded-[50%] hover:rotate-180 hover:rounded-md transition-all duration-300 ease-linear`
       ),
     [colors]
   );
