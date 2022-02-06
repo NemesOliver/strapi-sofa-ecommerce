7355a8430758032307f9522e2b61b114befc21ba
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-02-06 13:15:02 +0000

    feat: add comment for brevity

--- components/product/libs/color_picker/ColorPicker.js
+++ components/product/libs/color_picker/ColorPicker.js
@@ -2,20 +2,29 @@ import { useMemo } from "react";
 
 export const ColorPicker = ({ changeImage, colors }) => {
   // Map over pictures from response, and set colors of color picker square based on picuters
-  // Color must be set in CMS under 'caption' for this to work, example: caption: #<color>
+  // Color must be set in CMS under 'caption' for this to work, example: caption: #<HEX>
   const classNames = useMemo(
     () =>
-      colors.map(
-        ({ color }) =>
-          `w-[60px] h-[60px] bg-[${color}] mx-2 sm:mr-4 rounded-[50%] hover:rotate-180 hover:rounded-md transition-all duration-300 ease-linear`
-      ),
+      colors.map(({ color }) => ({
+        tailwind: `w-[60px] h-[60px] mx-2 sm:mr-4 rounded-[50%] hover:rotate-180 hover:rounded-md transition-all duration-300 ease-linear`,
+        custom: color,
+      })),
     [colors]
   );
 
   return (
     <div className="w-full flex justify-center mb-6 sm:block">
       {classNames.map((className, i) => (
-        <button onClick={() => changeImage(i)} key={i} className={className} />
+        <button
+          onClick={() => changeImage(i)}
+          key={i}
+          className={className.tailwind}
+          // Has to be passed inline for this to work, for some reason tailwind
+          // doesn't display colors right
+          style={{
+            backgroundColor: className.custom,
+          }}
+        />
       ))}
     </div>
   );
