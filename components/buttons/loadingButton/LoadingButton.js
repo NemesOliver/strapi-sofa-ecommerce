f2a612380ab14d7c4534084427f84710e5ffa540
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-02-13 14:01:02 +0000

    style: hover and active effect on loading button

--- components/buttons/loadingButton/LoadingButton.js
+++ components/buttons/loadingButton/LoadingButton.js
@@ -2,7 +2,7 @@ import { LoadingIcon } from "./libs";
 
 export const LoadingButton = ({ isLoading, text }) => {
   return (
-    <button className="w-full px-2 py-1.5 bg-primary rounded-sm mt-10 uppercase">
+    <button className="w-full px-2 py-1.5 bg-primary rounded-sm mt-10 uppercase hover:scale-95 active:scale-100 transition-all">
       {isLoading ? <LoadingIcon /> : text}
     </button>
   );
